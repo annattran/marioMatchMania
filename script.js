ba2d104443165112f002d321ad8c2ba9b9943b28
Anna Tran <52688715+annattran@users.noreply.github.com>
2024-03-26 11:34:57 -0400

    convert jquery to vanilla js

--- script.js
+++ script.js
@@ -1,50 +1,63 @@
-$(document).ready(function () {
-    const cards = Array.from($('body').find('li'));
-    // initial return is an object
-    // Array.from converts the object to an array
-
-
+document.addEventListener("DOMContentLoaded", () => {
     // GLOBAL VARIABLES
-    const cardImages = []; // to contain img names of all cards
+    const cards = document.querySelectorAll('.card');
     const backgroundMusic = document.getElementById("background");
     const soundButton = document.getElementById("sound");
+    let randomIntegers = []; // to contain integers that have been generated randomly
     let flippedCards = []; // to contain cards that are flipped
     let matchedCards = []; // to contain cards that are matched
     let flippedImages = []; // to contain img names that are flipped
 
 
     // FUNCTIONS --------------------------------------------------------------
-    // prevent ghost images from being dragged
-    $('li').on('dragstart', function (event) {
-        event.preventDefault();
-    });
+    function randomIntFromInterval(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
 
+    function countInArray(array, what) {
+        return array.filter(item => item == what).length;
+    }
+
+    function createRandomIntegers() {
+        while (randomIntegers.length < 6) {
+            const randomInt = randomIntFromInterval(1, 3);
+            if (countInArray(randomIntegers, randomInt) < 2) {
+                randomIntegers.push(randomInt);
+            }
+        }
+    }
+
+    function randomizeCards(card, index) {
+        const image = document.createElement('img');
+
+        image.src = `assets/card${randomIntegers[index]}.png`;
+
+        card.querySelector('.front.face').innerText = '';
+
+        card.querySelector('.front.face').append(image);
+    }
 
     // for each item in array 'cards'
-    cards.forEach(function (card) {
-        // find the img name of the item and push into cardImages array
-        cardImages.push($(card).find('img').attr('src'));
+    cards.forEach(function (card, index) {
 
-        // and add alt tags to the images with the specified attribute
-        $('img[src="assets/card1.png"]').attr('alt', 'Mario character from Mario franchise');
-        $('img[src="assets/card2.png"]').attr('alt', 'Red and white mushroom from Mario franchise');
-        $('img[src="assets/card3.png"]').attr('alt', 'Bowsy character from Mario franchise');
+        createRandomIntegers();
+        randomizeCards(card, index);
 
         // additionally, on the click of each item
-        $(card).on('click', function () {
+        card.addEventListener('click', () => {
             // add class of flipped
             card.classList.toggle('flipped');
             // run fireballSound
             fireballSound();
             // make the item unflippable
-            $(this).children().on('click', false);
+            card.style.pointerEvents = 'none';
             // log this card as flipped
-            flippedCards.push($(this));
+            flippedCards.push(card);
             // log the card's img name as flipped
-            flippedImages.push($(this).find('img').attr('src'));
+            flippedImages.push(card.querySelector('img').src);
             // look for elements with the class of 'matched'
             // and push into matchedCards array
-            matchedCards.push($('.matched'));
+            matchedCards.push(document.querySelectorAll('.matched'));
             // run disableFlipping and compareCards
             disableFlipping(compareCards);
         });
@@ -54,7 +67,9 @@ $(document).ready(function () {
     function disableFlipping(callback) {
         if (flippedImages.length % 2 === 0) {
             // if number of flipped images is perfectly divisible by 2
-            $('li').children().on('click', false);
+            cards.forEach(function (card) {
+                card.style.pointerEvents = 'none';
+            })
             // do not let anymore cards be flippable
         }
         callback();
@@ -62,8 +77,11 @@ $(document).ready(function () {
 
 
     function enableFlipping() {
-        $('li').not('.matched').children().off('click');
+        const unmatchedCards = document.querySelectorAll('.card:not(.matched)');
         // enable flipping on cards except cards with the class of 'matched'
+        unmatchedCards.forEach(function (card) {
+            card.style.pointerEvents = 'auto';
+        })
     }
 
 
@@ -74,21 +92,21 @@ $(document).ready(function () {
 
 
     function counter() {
-        let counter = parseInt($('.counter').text());
-        $('.counter').text(counter + 1);
+        let counter = parseInt(document.querySelector('.counter').innerText);
+        document.querySelector('.counter').innerText = counter + 1;
     }
 
 
     function totalMoves() {
-        let totalMoves = $('.totalMoves').text($('.counter').text());
+        document.querySelector('.totalMoves').innerText = document.querySelector('.counter').innerText;
     }
 
 
     function compareCards() {
         if (flippedImages.length === 2) {
             if (flippedImages[0] === flippedImages[1]) {
-                flippedCards[0].addClass('matched');
-                flippedCards[1].addClass('matched');
+                flippedCards[0].classList.add('matched');
+                flippedCards[1].classList.add('matched');
                 coinAnimation();
                 coinSound();
                 clearArrays();
@@ -99,8 +117,8 @@ $(document).ready(function () {
                 closeCongrats();
             } else {
                 setTimeout(function () {
-                    flippedCards[0].removeClass('flipped');
-                    flippedCards[1].removeClass('flipped');
+                    flippedCards[0].classList.remove('flipped');
+                    flippedCards[1].classList.remove('flipped');
                     clearArrays();
                     matchedCards.splice(-2);
                     enableFlipping();
@@ -119,15 +137,11 @@ $(document).ready(function () {
         }, 500);
     }
 
-    function cloudAnimation() {
-        $('body').append('<img class="single cloud" src="assets/singleCloud.png">');
-        $('body').append('<img class="double cloud" src="assets/doubleCloud.png">');
-    }
-
 
     function toggleSound() {
-        $(soundButton).on('click', function () {
-            $(soundButton).toggleClass('fa-volume-mute fa-volume-up');
+        soundButton.addEventListener('click', function () {
+            soundButton.classList.toggle('fa-volume-mute');
+            soundButton.classList.toggle('fa-volume-up');
             backgroundSound();
         })
     }
@@ -135,7 +149,7 @@ $(document).ready(function () {
 
     function fireballSound() {
         const fireball = document.getElementById("fireball");
-        if ($(sound).hasClass('fa-volume-mute')) {
+        if (sound.classList.contains('fa-volume-mute')) {
             fireball.pause();
         } else {
             fireball.play();
@@ -146,7 +160,7 @@ $(document).ready(function () {
     function coinSound() {
         const coin = document.getElementById("coin");
         setTimeout(function () {
-            if ($(sound).hasClass('fa-volume-mute')) {
+            if (sound.classList.contains('fa-volume-mute')) {
                 coin.pause();
             } else {
                 coin.play();
@@ -157,7 +171,7 @@ $(document).ready(function () {
 
     function winSound() {
         const win = document.getElementById("win");
-        if ($(sound).hasClass('fa-volume-mute')) {
+        if (sound.classList.contains('fa-volume-mute')) {
             win.pause();
         } else {
             win.play();
@@ -167,7 +181,7 @@ $(document).ready(function () {
 
     function backgroundSound() {
         backgroundMusic.loop = true;
-        if ($(sound).hasClass('fa-volume-mute')) {
+        if (sound.classList.contains('fa-volume-mute')) {
             backgroundMusic.pause();
         } else {
             backgroundMusic.play();
@@ -190,14 +204,17 @@ $(document).ready(function () {
 
 
     function clickQuestionBox() {
-        $('.question').on('click', function () {
+        document.querySelector('.question').addEventListener('click', function () {
             openInstructions();
         });
     }
 
 
     function closeCongrats() {
-        $('.congrats .close, .playAgain').on('click', function () {
+        document.querySelector('.congrats .close').addEventListener('click', function () {
+            document.getElementById('congratsModal').style.display = "none";
+        });
+        document.querySelector('.playAgain').addEventListener('click', function () {
             document.getElementById('congratsModal').style.display = "none";
         });
     }
@@ -209,7 +226,10 @@ $(document).ready(function () {
 
 
     function closeInstructions() {
-        $('.instructions .close, .ready').on('click', function () {
+        document.querySelector('.instructions .close').addEventListener('click', function () {
+            document.getElementById('instructionsModal').style.display = "none";
+        });
+        document.querySelector('.ready').addEventListener('click', function () {
             document.getElementById('instructionsModal').style.display = "none";
         });
     }
@@ -217,48 +237,26 @@ $(document).ready(function () {
 
     function playAgain() {
         // on click of playAgain
-        $('.playAgain').on('click', function () {
+        document.querySelector('.playAgain').addEventListener('click', function () {
             // reset counter to 0
-            $('.counter').text(0);
+            document.querySelector('.counter').innerText = 0;
             // remove 'flipped' & 'matched' classes off all cards
-            $('li').removeClass('flipped matched');
+            randomIntegers = [];
+            cards.forEach(function (card, index) {
+                card.classList.remove('flipped', 'matched');
+                createRandomIntegers();
+                randomizeCards(card, index);
+            })
             // reset array of matchedCards
             matchedCards = [];
             // run the following functions
             enableFlipping();
             backgroundSound();
-            shuffle(cardImages);
-            displayShuffle();
         });
     }
 
 
-    // Fisher-Yates (aka Knuth) Shuffle. (NOT MY CODE).
-    function shuffle(array) {
-        let currentIndex = array.length, temporaryValue, randomIndex;
-
-        while (0 !== currentIndex) {
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex = currentIndex - 1;
-            temporaryValue = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temporaryValue;
-        }
-        return array;
-    }
-
-
-    function displayShuffle() {
-        const shuffledCards = shuffle(cardImages);
-        for (i = 0; i < shuffledCards.length; i++) {
-            let newImage = `<img src="${shuffledCards[i]}">`
-            $(`.front.${i}`).html(newImage);
-        }
-    }
-
-
     // FUNCTIONS RUNNED ON DOCUMENT READY
-    cloudAnimation();
     toggleSound();
     clickQuestionBox();
     openInstructions();
